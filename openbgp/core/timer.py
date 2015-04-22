2e3b57972daa117b3702b308836aa38fee6e9e45
Xiao Peng <penxiao@cisco.com>
2015-04-22 15:03:24 +0800

    change copy right for some files

--- openbgp/core/timer.py
+++ openbgp/core/timer.py
@@ -1,5 +1,4 @@
-# Copyright 2015 Cisco Systems, Inc.
-# All rights reserved.
+# Copyright (c) 2007 by Mark Bergsma <mark@nedworks.org>
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
