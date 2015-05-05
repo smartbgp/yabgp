195caee293ba990f2b43b4c473bf275dc2eba737
Xiao Peng <penxiao@cisco.com>
2015-05-05 15:22:59 +0800

    update hook.py for setup.py

--- openbgp/hooks.py
+++ openbgp/hooks.py
@@ -13,9 +13,19 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+# copy from # get from https://github.com/osrg/ryu/blob/master/ryu/hooks.py
 import sys
+from setuptools.command import easy_install
 
 from openbgp import version
+
+
+# Global variables in this module doesn't work as we expect
+# because, during the setup procedure, this module seems to be
+# copied (as a file) and can be loaded multiple times.
+# We save them into __main__ module instead.
+def _main_module():
+    return sys.modules['__main__']
 
 
 def setup_hook(config):
@@ -28,3 +38,26 @@ def setup_hook(config):
     metadata['requires_dist'] = "\n".join(requires)
     config['metadata'] = metadata
     metadata['version'] = str(version)
+
+    # pbr's setup_hook replaces easy_install.get_script_args with
+    # their own version, override_get_script_args, prefering simpler
+    # scripts which are not aware of multi-version.
+    # prevent that by doing the opposite.  it's a horrible hack
+    # but we are in patching wars already...
+    from pbr import packaging
+
+    def my_get_script_args(*args, **kwargs):
+        return _main_module()._orig_get_script_args(*args, **kwargs)
+
+    packaging.override_get_script_args = my_get_script_args
+    easy_install.get_script_args = my_get_script_args
+
+    # another hack to allow setup from tarball.
+    orig_get_version = packaging.get_version
+
+    def my_get_version(package_name, pre_version=None):
+        if package_name == 'openbgp':
+            return str(version)
+        return orig_get_version(package_name, pre_version)
+
+    packaging.get_version = my_get_version
