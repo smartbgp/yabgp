06624b815408d680bf332c93de8c63f6063fda3d
Peng Xiao <xiaoquwl@gmail.com>
2015-06-09 17:29:45 +0800

    add basic http auth to rest api version 1

--- yabgp/api/utils.py
+++ yabgp/api/utils.py
@@ -21,24 +21,45 @@ from yabgp.common import constants as common_cons
 
 
 def get_peer_conf_and_state(peer_ip=None):
-    if peer_ip:
-        pass
-        return
-    result = {
-        'peers': []
-    }
-    for peer in cfg.CONF.bgp.running_config:
-        peer_state = {
-            'remote_as': cfg.CONF.bgp.running_config[peer]['remote_as'],
-            'local_addr': cfg.CONF.bgp.running_config[peer]['local_addr'],
-            'local_as': cfg.CONF.bgp.running_config[peer]['local_as'],
-            'remote_addr': cfg.CONF.bgp.running_config[peer]['remote_addr']
-        }
+    """
+    get peer configuration and state
+    :param peer_ip: peer ip address
+    :return:
+    """
+    def get_peer_state(peer):
+        one_peer_state = {key: cfg.CONF.bgp.running_config[peer][key] for key in [
+            'remote_as', 'remote_addr', 'local_as', 'local_addr']}
         fsm = cfg.CONF.bgp.running_config[peer]['factory'].fsm.state
-        peer_state['fsm'] = common_cons.stateDescr[fsm]
+        one_peer_state['fsm'] = common_cons.stateDescr[fsm]
         if fsm == common_cons.ST_ESTABLISHED:
-            peer_state['uptime'] = time.time() - cfg.CONF.bgp.running_config[peer]['factory'].fsm.uptime
+            one_peer_state['uptime'] = time.time() - cfg.CONF.bgp.running_config[peer]['factory'].fsm.uptime
         else:
-            peer_state['uptime'] = 0
-        result['peers'].append(peer_state)
+            one_peer_state['uptime'] = 0
+        return one_peer_state
+
+    if peer_ip:
+        peer_state = {}
+        if peer_ip in cfg.CONF.bgp.running_config:
+            peer_state = get_peer_state(peer_ip)
+        return {'peer': peer_state}
+    # for multi peers
+    result = {'peers': []}
+    for peer_ip in cfg.CONF.bgp.running_config:
+        result['peers'].append(get_peer_state(peer_ip))
     return result
+
+
+def get_peer_msg_statistic(peer_ip=None):
+    """
+    get peer send and receive message statistic
+    :param peer_ip: peer ip address
+    :return:
+    """
+
+    if peer_ip and peer_ip in cfg.CONF.bgp.running_config:
+        return {
+            'send': cfg.CONF.bgp.running_config[peer_ip]['factory'].fsm.protocol.msg_sent_stat,
+            'receive': cfg.CONF.bgp.running_config[peer_ip]['factory'].fsm.protocol.msg_recv_stat,
+        }
+    else:
+        return {}
--- yabgp/api/v1.py
+++ yabgp/api/v1.py
@@ -17,13 +17,23 @@
 """
 import logging
 
+from flask.ext.httpauth import HTTPBasicAuth
 from flask import Blueprint
 import flask
+from oslo.config import cfg
 
 from yabgp.api import utils as api_utils
 
 LOG = logging.getLogger(__name__)
 blueprint = Blueprint('v1', __name__)
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_pw(username):
+    if username == cfg.CONF.rest.username:
+        return cfg.CONF.rest.password
+    return None
 
 
 @blueprint.route('/')
@@ -37,7 +47,7 @@ def root():
 
       GET /v1 HTTP/1.1
       Host: example.com
-      Accept: application/json, text/javascript
+      Accept: application/json
 
     **Example response**:
 
@@ -62,9 +72,10 @@ def root():
 
 
 @blueprint.route('/peers',  methods=['GET'])
+@auth.login_required
 def peers():
     """
-    Get all peers realtime running information.
+    Get all peers realtime running information, include basic configurations and fsm state.
 
     **Example request**
 
@@ -72,7 +83,7 @@ def peers():
 
       GET /v1/peers HTTP/1.1
       Host: example.com
-      Accept: application/json, text/javascript
+      Accept: application/json
 
     **Example response**:
 
@@ -108,12 +119,85 @@ def peers():
     return flask.jsonify(api_utils.get_peer_conf_and_state())
 
 
-@blueprint.route('/peer/<peer_ip>')
+@blueprint.route('/peer/<peer_ip>/state')
+@auth.login_required
 def peer(peer_ip):
     """
-    Get one peer's running information.
+    Get one peer's running information, include basic configurations and fsm state.
+
+    **Example request**
+
+    .. sourcecode:: http
+
+      GET /v1/peer/10.124.1.245/state HTTP/1.1
+      Host: example.com
+      Accept: application/json
+
+    **Example response**:
+
+    .. sourcecode:: http
+
+      HTTP/1.1 200 OK
+      Vary: Accept
+      Content-Type: text/json
+      {
+        "peer": {
+            "fsm": "ESTABLISHED",
+            "local_addr": "10.75.44.11",
+            "local_as": 23650,
+            "remote_addr": "10.124.1.245",
+            "remote_as": 23650,
+            "uptime": 7.913731813430786
+            }
+        }
 
     :param peer_ip: peer ip address
+    :status 200: the api can work.
 
     """
-    pass
+    return flask.jsonify(api_utils.get_peer_conf_and_state(peer_ip))
+
+
+@blueprint.route('/peer/<peer_ip>/statistic')
+@auth.login_required
+def get_peer_statistic(peer_ip):
+    """
+    Get one peer's message statistic, include sending and receiving.
+
+    **Example request**
+
+    .. sourcecode:: http
+
+      GET /v1/peer/10.124.1.245/statistic HTTP/1.1
+      Host: example.com
+      Accept: application/json
+
+    **Example response**:
+
+    .. sourcecode:: http
+
+      HTTP/1.1 200 OK
+      Vary: Accept
+      Content-Type: text/json
+      {
+          "receive": {
+              "Keepalives": 3,
+              "Notifications": 0,
+              "Opens": 1,
+              "Route Refresh": 0,
+              "Updates": 5
+          },
+          "send": {
+              "Keepalives": 3,
+              "Notifications": 0,
+              "Opens": 1,
+              "Route Refresh": 0,
+              "Updates": 0
+          }
+      }
+
+    :param peer_ip: peer ip address
+    :status 200: the api can work.
+
+    """
+    return flask.jsonify(api_utils.get_peer_msg_statistic(peer_ip))
