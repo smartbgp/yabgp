e0d9576783c935b06692af533c49880dc0650ffc
Peng Xiao <xiaoquwl@gmail.com>
2015-08-04 17:05:37 +0800

    add more actions when starting rabbitmq and mongodb
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- tools/rabbit_mongo.sh
+++ tools/rabbit_mongo.sh
@@ -19,14 +19,20 @@ echo "Please makesure you have installed docker and can run as no-root user"
 echo "if yes, please press Enter..."
 read
 
+# try to stop running containers
+docker kill `docker ps | grep mongo | awk '{print $1}'`
+docker kill `docker ps | grep rabbit | awk '{print $1}'`
+
+# clean containers
+docker rm `docker ps -a | awk '{print $1}'`
+
+
 # for rabbitmq
 echo "Starting rabbitmq..."
-docker pull tutum/rabbitmq
 docker run -d -p 5672:5672 -p 15672:15672 tutum/rabbitmq
 
 # for mongodb
 echo "starting mongodb replica set"
-docker pull tattsun/mongodb-replset
 
 cd ~
 for ((i=1; i<4; i++)); do
@@ -40,3 +46,15 @@ SCRIPTPATH=`pwd -P`
 docker run -p 27017:27017 -p 27018:27018 -p 27019:27019 -d \
     -v /host/primary:$SCRIPTPATH/data/mongo/node1 -v /host/secondary:$SCRIPTPATH/data/mongo/node2 \
     tattsun/mongodb-replset
+
+sleep 30
+
+echo "init mongodb replca set"
+
+mongo admin --eval "rs.initiate()"
+
+mongo_host_id=`docker ps | grep mongo | awk '{print $1}'`
+
+mongo admin --eval "rs.add(\"$mongo_host_id:27018\")"
+
+mongo admin --eval "rs.add(\"$mongo_host_id:27019\")"
