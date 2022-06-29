631d9c2cb3a10b13de3e04320e9e74f8c271ddf0
远哥制造 <yuangezhizao@icloud.com>
2022-06-29 11:07:24 +0800

    Add SRv6 End.X SID (type 1106) & SRv6 SID Structure (type 1252) (#135)
    * ✨ Feat(linkstate/link/srv6_end_x_sid.py): add SRv6 End.X SID (type 1106) & SRv6 SID Structure (type 1252)
    
    * 👌 Review(srv6_end_x_sid.py): remove "reserved" in the return value

---
+++ yabgp/message/attribute/linkstate/link/srv6_end_x_sid.py
@@ -0,0 +1,65 @@
+# Copyright 2022 Cisco Systems, Inc.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import binascii
+import struct
+
+import netaddr
+
+from yabgp.tlv import TLV
+from ..linkstate import LinkState
+
+
+@LinkState.register()
+class SRv6EndXSid(TLV):
+    """
+    SRv6 End.X SID
+    """
+    TYPE = 1106  # https://datatracker.ietf.org/doc/html/draft-ietf-idr-bgpls-srv6-ext-09#section-4.1
+    TYPE_STR = 'srv6_end_x_sid'
+
+    @classmethod
+    def unpack(cls, data):
+        endpoint_behavior = struct.unpack('!H', data[:2])[0]
+        flags = ord(data[2:3])
+        algorithm = ord(data[3:4])
+        weight = ord(data[4:5])
+        # reserved = ord(data[5:6])
+        sid = str(netaddr.IPAddress(int(binascii.b2a_hex(data[6:22]), 16)))
+        sub_tlvs_bin_data = data[22:]
+
+        sub_tlvs = []
+        while sub_tlvs_bin_data:
+            sub_tlvs_type_code, sub_tlvs_length = struct.unpack('!HH', sub_tlvs_bin_data[:4])
+            sub_tlvs_value = sub_tlvs_bin_data[4: 4 + sub_tlvs_length]
+
+            if sub_tlvs_type_code in LinkState.registered_tlvs:
+                # Note: "sub_tlvs_value" does not contains "sub_tlvs_type_code" & "sub_tlvs_length"
+                sub_tlvs.append(LinkState.registered_tlvs[sub_tlvs_type_code].unpack(sub_tlvs_value).dict())
+            else:
+                sub_tlvs.append({
+                    'type': sub_tlvs_type_code,
+                    'value': str(binascii.b2a_hex(sub_tlvs_value))
+                })
+            sub_tlvs_bin_data = sub_tlvs_bin_data[4 + sub_tlvs_length:]
+
+        return cls(value={
+            'endpoint_behavior': endpoint_behavior,
+            'flags': flags,
+            'algorithm': algorithm,
+            'weight': weight,
+            'sid': sid,
+            'sub_tlvs': sub_tlvs
+        })
---
+++ yabgp/tests/unit/message/attribute/linkstate/link/test_srv6_end_x_sid.py
@@ -0,0 +1,62 @@
+# Copyright 2022 Cisco Systems, Inc.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+""" Test SRv6 End.X SID """
+
+import unittest
+
+from yabgp.message.attribute.linkstate.link.srv6_end_x_sid import SRv6EndXSid
+
+
+class TestSRv6EndXSid(unittest.TestCase):
+
+    def test_unpack(self):
+        data_bin = b'\x04\x52' \
+                   b'\x00\x1e' \
+                   b'\x00\x39' \
+                   b'\x00' \
+                   b'\x00' \
+                   b'\x00' \
+                   b'\x00' \
+                   b'\xa0\x01\x00\x00\x00\x05\xe0\x02\x00\x00\x00\x00\x00\x00\x00\x00' \
+                   b'\x04\xe4' \
+                   b'\x00\x04' \
+                   b'\x20\x10\x10\x00'
+
+        data_dict = {
+            'type': 'srv6_end_x_sid',
+            'value': {
+                'endpoint_behavior': 57,
+                'flags': 0,
+                'algorithm': 0,
+                'weight': 0,
+                'sid': 'a001:0:5:e002::',
+                'sub_tlvs': [
+                    {
+                        'type': 'srv6_sid',
+                        'value': {
+                            'locator_block_length': 32,
+                            'locator_node_length': 16,
+                            'function_length': 16,
+                            'argument_length': 0
+                        }
+                    }
+                ]
+            }
+        }
+        # The total of the locator block, locator node, function, and argument lengths MUST be less than or equal to 128
+
+        # Note: data_bin[4:] means does not contains "type_code" & "length"
+        self.assertEqual(data_dict, SRv6EndXSid.unpack(data_bin[4:]).dict())
