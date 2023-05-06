f36432ab213768eac35a9d06bd4240ba4133e7e7
远哥制造 <yuangezhizao@icloud.com>
2023-05-06 15:49:24 +0800

    🚑 Quickfix(ipv6_unicast.py): fix NLRI ipv6_unicast decoding, close #153 (#154)

--- yabgp/tests/unit/message/attribute/nlri/test_ipv6_unicast.py
+++ yabgp/tests/unit/message/attribute/nlri/test_ipv6_unicast.py
@@ -28,6 +28,12 @@ class TestIPv6Unicast(unittest.TestCase):
         value_hoped = ['2001:3232::1/128', '2001:3232:1::/64', '2001:4837:1632::2/127']
         self.assertEqual(value_hoped, value_parsed)
 
+    def test_parse_2(self):
+        nlri_data = b'\x70\x32\x6c\xce\x92\x25\xea\x36\x5e\x4d\x71\x59\x45\x22\x00\x00\x00'
+        value_parsed = IPv6Unicast().parse(nlri_data)
+        value_hoped = ['326c:ce92:25ea:365e:4d71:5945:2200:0/112']
+        self.assertEqual(value_hoped, value_parsed)
+
     def test_parse_withdraw(self):
         nlri_data = b'\x40\x20\x01\x0d\xb8\x00\x01\x00\x02\x40\x20\x01\x0d\xb8\x00\x01\x00\x01\x40\x20\x01\x0d' \
                     b'\xb8\x00\x01\x00\x00'
@@ -47,6 +53,12 @@ class TestIPv6Unicast(unittest.TestCase):
         value_hex = IPv6Unicast.construct(nlri_list)
         value_hoped = b'\x80\x20\x01\x32\x32\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x40\x20\x01\x32' \
                       b'\x32\x00\x01\x00\x00\x7f\x20\x01\x48\x37\x16\x32\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02'
+        self.assertEqual(value_hoped, value_hex)
+
+    def test_construct_nlri_3(self):
+        nlri_list = ['326c:ce92:25ea:365e:4d71:5945:2200:0/112']
+        value_hex = IPv6Unicast.construct(nlri_list)
+        value_hoped = b'\x70\x32\x6c\xce\x92\x25\xea\x36\x5e\x4d\x71\x59\x45\x22\x00'
         self.assertEqual(value_hoped, value_hex)
 
 
